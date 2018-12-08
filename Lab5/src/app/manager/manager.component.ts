5023562f42f527beb9e4d8788c36478d45121ccb
David Kezi <davidkezi6@gmail.com>
2018-12-09 00:39:11 +0100

    idk

--- Lab5/src/app/manager/manager.component.ts
+++ Lab5/src/app/manager/manager.component.ts
@@ -1,42 +1,63 @@
-import { Component, OnInit, EventEmitter, Input, Output, ViewChild } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { ProviderAst } from '@angular/compiler';
-import { MatTableDataSource, } from '@angular/material';
-import { map } from 'rxjs/operators';
-import { User } from './user.model';
-import { HttpHeaders } from '@angular/common/http';
-import { Observable, of, from } from 'rxjs';
-import { DataSource } from '@angular/cdk/collections';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatButtonModule } from '@angular/material/button';
-import { TableDataSource, ValidatorService } from 'angular4-material-table';
-import { ManagerService} from '../services/manager.service';
-import { ManagerValidatorService} from './manager-validator.service';
+import {
+  Component,
+  OnInit,
+  EventEmitter,
+  Input,
+  Output,
+  ViewChild
+} from "@angular/core";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { ProviderAst } from "@angular/compiler";
+import { MatTableDataSource } from "@angular/material";
+import { map } from "rxjs/operators";
+import { User } from "./user.model";
+import { HttpHeaders } from "@angular/common/http";
+import { Observable, of, from } from "rxjs";
+import { DataSource } from "@angular/cdk/collections";
+import { MatPaginator } from "@angular/material/paginator";
+import { MatButtonModule } from "@angular/material/button";
+import { TableDataSource, ValidatorService } from "angular4-material-table";
+import { ManagerService } from "../services/manager.service";
+import { ManagerValidatorService } from "./manager-validator.service";
 
 @Component({
-  selector: 'app-manager',
-  templateUrl: './manager.component.html',
-  styleUrls: ['./manager.component.css'],
-  providers : [ ManagerService, ManagerValidatorService]
+  selector: "app-manager",
+  templateUrl: "./manager.component.html",
+  styleUrls: ["./manager.component.css"],
+  providers: [ManagerService, ManagerValidatorService]
 })
 export class ManagerComponent implements OnInit {
-  constructor(private managerService: ManagerService, private managerValidatorService: ManagerValidatorService) { }
+  user: User[];
+  constructor(
+    private managerService: ManagerService,
+    private managerValidatorService: ManagerValidatorService
+  ) {}
   dataSource;
-  displayedColumns = ['Item', 'price', 'Quantity', 'Tax', 'actions'];
+  displayedColumns = ["Item", "price", "Quantity", "Tax", "actions"];
   @Input() appComponent = this.dataSource;
   @Output() AppComponentChange = new EventEmitter<User[]>();
   @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  cancelOrDelete(s) {
+    console.log(s);
+  }
+
   ngOnInit() {
     this.managerService.getUser().subscribe((data: User[]) => {
       console.log(data);
       this.dataSource = data;
-      this.dataSource.paginator = this.paginator ;
+      this.dataSource.paginator = this.paginator;
     });
   }
+  updateUser(user) {
+    this.managerService
+      .updateUser(user)
+      .subscribe(user => this.user.push(user));
+  }
 }
-    export class UserDataSource extends MatTableDataSource<any> {
-      constructor(private managerService: ManagerService) {
-        super();
-      }
-      disconnect() { }
-    }
+export class UserDataSource extends MatTableDataSource<any> {
+  constructor(private managerService: ManagerService) {
+    super();
+  }
+  disconnect() {}
+}
--- Lab5/src/app/services/manager.service.ts
+++ Lab5/src/app/services/manager.service.ts
@@ -1,21 +1,35 @@
-import { Injectable } from '@angular/core';
-import { map } from 'rxjs/operators';
-import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
-import { HttpHeaders } from '@angular/common/http';
-import { Type } from '@angular/compiler';
-import { User } from '../manager/user.model'; 
+import { Injectable } from "@angular/core";
+import { map } from "rxjs/operators";
+import { Observable } from "rxjs";
+import { HttpClient } from "@angular/common/http";
+import { HttpHeaders } from "@angular/common/http";
+import { Type } from "@angular/compiler";
+import { User } from "../manager/user.model";
 
-
+const httpOptions = {
+  headers: new HttpHeaders({
+    "Content-Type": "application/json"
+  })
+};
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class ManagerService {
-  private serviceUrl = 'http://localhost:7070/products/test';
-  private posturl = 'http://localhost:7070/products/create';
-  constructor(private http: HttpClient) { }
+  private serviceUrl = "http://localhost:7070/products/test";
+  private posturl = "http://localhost:7070/products/create";
+  private putUrl = "http://localhost:7070/products";
+  constructor(private http: HttpClient) {}
   getUser() {
     return this.http.get<User[]>(this.serviceUrl);
   }
-
+  updateUser(user: User): Observable<User> {
+    // httpOptions.headers = httpOptions.headers.set(
+    //   "Authorization",
+    //   "my-new-auth-token"
+    // );
+    const url = `${this.putUrl}/${user["_id"]}`;
+    console.log(url);
+    console.log(user);
+    return this.http.put<User>(url, user);
+  }
 }
